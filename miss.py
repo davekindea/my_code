422a2716a3bd342d3147d5dba9a0d42ed445f0ba
davekindea <110993270+davekindea@users.noreply.github.com>
2023-07-28 10:08:32 +0300

    Create miss.py

---
+++ miss.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        x=len(nums)
+        for i in range (x+1):
+            if i not in nums:
+                return i
+                break
+            else:
+                continue
+        
